c9c3b01c6f0ef51f3319857729bb1fe3ec16d54c
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2022-10-04 11:41:44 +0200

    Add feature test (invoices-5)

--- tests/Feature/Invoice/ListInvoiceTest.php
+++ tests/Feature/Invoice/ListInvoiceTest.php
@@ -12,7 +12,7 @@ class ListInvoiceTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_invoices_of_current_user_can_be_listed()
+    public function test_invoices_can_be_listed()
     {
         $user = User::factory()->create();
         $this->actingAs($user);
@@ -32,5 +32,26 @@ class ListInvoiceTest extends TestCase
                 ]
             ]
         ]);
+    }
+
+    public function test_current_user_can_retrieve_only_his_invoices()
+    {
+        $user = User::factory()->create();
+        Invoice::factory(10)->create([
+            'user_id' => $user->id
+        ]);
+
+        $another_user = User::factory()->create();
+        Invoice::factory(10)->create([
+            'user_id' => $another_user->id
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get('/api/invoices');
+
+        $this->assertDatabaseCount('invoices', 20);
+        $response->assertJsonCount(10, 'data');
+        $response->assertStatus(200);
     }
 }
