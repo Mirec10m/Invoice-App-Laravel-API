8994061f23ef6d364938813c296496f883727439
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2023-03-16 16:21:18 +0100

    Edit Invoice validation rules (develop)

--- app/Http/Requests/Invoice/CreateInvoiceRequest.php
+++ app/Http/Requests/Invoice/CreateInvoiceRequest.php
@@ -26,9 +26,14 @@ class CreateInvoiceRequest extends FormRequest
         return [
             'number' => 'required|digits_between:5,20|unique:invoices,number',
             'variable_symbol' => 'nullable|numeric|min:6',
+            'issued_at' => 'nullable|date_format:Y-m-d',
+            'delivered_at' => 'nullable|date_format:Y-m-d',
             'due_at' => 'nullable|date_format:Y-m-d',
             'item' => 'nullable|string|max:255',
+            'quantity' => 'nullable|integer|numeric',
+            'unit' => 'nullable|string|max:255',
             'price' => 'nullable|numeric|between:0.00,99999999.99',
+            'sum' => 'nullable|numeric|between:0.00,99999999.99',
             'customer_id' => 'nullable|exists:customers,id'
         ];
     }
