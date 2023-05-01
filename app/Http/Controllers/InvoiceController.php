6feb3fc8e310aca62c9dc4ce53443e7540ee15ad
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2023-05-01 13:26:17 +0200

    Add filters to Invoices index (develop)

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -23,7 +23,19 @@ class InvoiceController extends Controller
 
     public function index()
     {
-        $invoices = Invoice::where('user_id', Auth::user()->id)->with(['user', 'customer'])->paginate(20);
+        $invoices = Invoice::where('user_id', Auth::user()->id)->with(['user', 'customer']);
+
+        if($filter = request()->input('filter')){
+            $invoices = $invoices->where(function($q) use ($filter){
+                $q->where('number', 'LIKE', '%' . $filter . '%')
+                    ->orWhere('sum', 'LIKE', '%' . $filter . '%')
+                    ->orWhereHas('customer', function($q) use ($filter) {
+                        $q->where('name', 'LIKE', '%' . $filter . '%');
+                    });
+            });
+        }
+
+        $invoices = $invoices->paginate(20);
 
         return InvoiceResource::collection($invoices);
     }
